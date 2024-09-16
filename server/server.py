efd393c4cb8a154a0d07845cce617f9f9a0999f9
Stan Tsouvallas <stan@stately.cloud>
2024-09-16 13:18:28 +1000

    Bug fixes

--- server/server.py
+++ server/server.py
@@ -5,6 +5,7 @@ import asyncio
 import json
 from collections import defaultdict
 from websockets.asyncio.server import serve, ServerConnection
+from websockets.exceptions import ConnectionClosedOK
 from dataclasses import dataclass
 from typing import Any
 from datetime import datetime
@@ -36,58 +37,99 @@ async def broadcast(room_id: str, sender: str, message: Any):
     ]
 
     for user in broadcast_users:
-        await user.connection.send(json.dumps(message))
+        try:
+            await user.connection.send(json.dumps(message))
+        except ConnectionClosedOK:
+            print(f"Broadcast failed because user: {user.username} disconnected")
+            await remove_user(room_id, user.username)
 
 
-async def send_to(room_id: str, sender: str, recipient: str, message: Any):
+async def send_to(room_id: str, recipient: str, message: Any):
     user = rooms[room_id].users[recipient]
     await user.connection.send(json.dumps(message))
 
 
-async def handler(websocket: ServerConnection) -> None:
-    # wait for the initial message
-    initial_message_json = await websocket.recv()
-    initial_message = json.loads(initial_message_json)
+async def add_user(room_id: str, username: str, connection: ServerConnection):
+    if username in rooms[room_id].users:
+        print(f"User {username} already in room {room_id}. Booting old user")
+        await remove_user(room_id, username)
 
-    # make sure it's a join message
-    if initial_message["type"] != "join":
-        await websocket.close()
-        print(f"initial message is not join. was {initial_message}. booting user.")
-        return
-
-    # add the user to the room
-    room_id = initial_message["room"]
-    username = initial_message["username"]
-    print(f"User {username} joined room {room_id}")
+    # overwrite the user
     rooms[room_id].users[username] = UserState(
-        username=username, connection=websocket, joined=datetime.now()
+        username=username, connection=connection, joined=datetime.now()
     )
-
+    print(
+        f"User {username} joined room {room_id}: {", ".join(rooms[room_id].users.keys())}"
+    )
     # tell the other users about the new user
     await broadcast(room_id, username, {"type": "user_joined", "username": username})
 
-    async for msg_json in websocket:
-        msg = json.loads(msg_json)
-        if msg["type"] == "sdp":
-            to_username = msg["to"]
-            del msg["to"]
-            msg["from"] = username
-            msg["polite"] = (
-                rooms[room_id].users[to_username].joined
-                > rooms[room_id].users[username].joined
-            )
-            await send_to(room_id, username, to_username, msg)
 
-    print(f"User {username} left room {room_id}")
-    del rooms[room_id].users[username]
+async def remove_user(room_id: str, username: str):
+    print(f"Removing user {username} from room {room_id}")
+
+    if username in rooms[room_id].users:
+        await rooms[room_id].users[username].connection.close()
+        del rooms[room_id].users[username]
     await broadcast(room_id, username, {"type": "user_left", "username": username})
     # remove the room if it's empty
     if len(rooms[room_id].users) == 0:
         del rooms[room_id]
 
 
+async def handle_sdp(room_id: str, username: str, msg: dict[str, Any]):
+    recipient = msg["to"]
+    del msg["to"]
+    msg["from"] = username
+    msg["polite"] = (
+        rooms[room_id].users[recipient].joined > rooms[room_id].users[username].joined
+    )
+    await send_to(room_id, recipient, msg)
+
+
+async def handler(websocket: ServerConnection) -> None:
+    # wait for the initial message
+    while True:
+        try:
+            initial_message_json = await websocket.recv()
+        except ConnectionClosedOK:
+            print("User disconnected before sending initial message")
+            return
+
+        # make sure we get a join message first
+        initial_message = json.loads(initial_message_json)
+        if initial_message["type"] == "ping":
+            # this is just the keepalive
+            # we can ignore it
+            continue
+        elif initial_message["type"] != "join":
+            await websocket.close()
+            print(f"initial message is not join. was {initial_message}. booting user.")
+            return
+        else:
+            # get the user's info
+            username = initial_message["username"]
+            room_id = initial_message["room"]
+            break
+
+    # add the user to the room
+    await add_user(room_id, username, websocket)
+
+    try:
+        async for msg_json in websocket:
+            msg = json.loads(msg_json)
+            if msg["type"] == "sdp":
+                await handle_sdp(room_id, username, msg)
+    except ConnectionClosedOK:
+        print(f"User {username} disconnected")
+    except Exception as e:
+        print(f"Error for user {username}: {e}")
+    finally:
+        await remove_user(room_id, username)
+
+
 async def main():
-    async with serve(handler, "localhost", 8765):
+    async with serve(handler, "localhost", 8765, ping_interval=10, ping_timeout=5):
         # run forever
         try:
             await asyncio.get_running_loop().create_future()
